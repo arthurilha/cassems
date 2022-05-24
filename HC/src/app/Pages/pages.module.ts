bdbcb3f7a39c229751a4bf41200ffbdb648726eb
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2022-05-24 17:02:04 -0300

    second

--- HC/src/app/Pages/pages.module.ts
+++ HC/src/app/Pages/pages.module.ts
@@ -5,11 +5,13 @@ import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PagesRoutingModule } from './pages-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent,ProfileComponent,HomeComponent],
+  declarations: [LoginComponent,ProfileComponent,HomeComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
