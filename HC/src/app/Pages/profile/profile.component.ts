c2eabcdd44ea4508b15cefa09ab6110f87eb8598
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2022-05-25 13:00:05 -0300

    second

--- HC/src/app/Pages/profile/profile.component.ts
+++ HC/src/app/Pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  form = new FormGroup({
+    nome: new FormControl({value: '', disabled: true}),
+    endereco: new FormControl({value: '', disabled: true}),
+    dataNascimento: new FormControl({value: '', disabled: true}),
+    email: new FormControl({value: '', disabled: true}),
+    telefone: new FormControl(null, [Validators.required]),
+  });
   constructor() { }
 
   ngOnInit(): void {
+  }
+  postContact(){
+    
   }
 
 }
